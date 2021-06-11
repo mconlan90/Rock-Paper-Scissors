81b41233a0bbf20d802c352dd85c905363db75d8
Matthew Conlan <matthewconlan1@gmail.com>
2021-06-11 09:21:17 +0200

    scoreboard is now working

--- rps.js
+++ rps.js
@@ -1,59 +1,66 @@
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
-const h1 = document.querySelector("h1");
 const result = document.querySelector("#result");
-let playerSelection;
-let computerSelection;
+const playerScore = document.querySelector('#playerScore');
+const computerScore = document.querySelector('#computerScore');
+let p1Score = 0;
+let p2Score = 0;
 
 rock.addEventListener('click', function() {
-    playerSelection = "rock";
-    computerSelection = computerPlay();
-    result.innerText = playRound(playerSelection, computerSelection);
-})
+    playButton("rock");
+});
 
 paper.addEventListener('click', function() {
-    playerSelection = "paper";
-    computerSelection = computerPlay();
-    result.innerText = playRound(playerSelection, computerSelection);
+    playButton("paper"); 
 });
 
 scissors.addEventListener('click', function() {
-    playerSelection = "scissors";
-    computerSelection = computerPlay();
-    result.innerText = playRound(playerSelection, computerSelection);
-})
+    playButton("scissors");
+});
+
+function playButton(playerSelection) {
+    result.innerText = playRound(playerSelection, computerPlay());
+};
 
 function computerPlay() {
     let options = ["Rock", "Paper", "Scissors"];
     let randomIndex = Math.floor(Math.random() * options.length);
     let randomOption = options[randomIndex];
     return randomOption;
-  }  
+  };  
 
-function displayPlayerWin() {
-    return "You win! " + playerSelection + " beats " + computerSelection + "!";
-    }
+  function capitalise(text) {
+    return text[0].toUpperCase() + text.slice(1);
+  };
+
+function displayPlayerWin(playerSelection, computerSelection) {
+    return "You win! " + capitalise(playerSelection) + " beats " + computerSelection + "!";
+    };
     
-function displayPlayerLose() {
-    return "You lose! " + computerSelection + " beats " + playerSelection + "!";
-    }
+function displayPlayerLose(playerSelection, computerSelection) {
+    return "You lose! " + computerSelection + " beats " + capitalise(playerSelection) + "!";
+    };
 
 function playRound (playerSelection, computerSelection) {
       
     if ( (playerSelection === "rock" && computerSelection === "Scissors") ||
         (playerSelection === "scissors" && computerSelection === "Paper") ||
         (playerSelection === "paper" && computerSelection === "Rock") ) {
-      return displayPlayerWin();
+        p1Score += 1;
+        playerScore.textContent = p1Score;
+      return displayPlayerWin(playerSelection, computerSelection);
     } else if 
         ( (playerSelection === "scissors" && computerSelection === "Rock") ||
         (playerSelection === "paper" && computerSelection === "Scissors") ||
         (playerSelection === "rock" && computerSelection === "Paper") )  {
-          return displayPlayerLose();
+        p2Score += 1;
+        computerScore.textContent = p2Score;
+          return displayPlayerLose(playerSelection, computerSelection);
       } else {
         return "It's a tie!";
       }
-    }
+    };
 
 
     
